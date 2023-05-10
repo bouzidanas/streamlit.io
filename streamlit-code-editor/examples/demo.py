5fccea8ea6e064e3350413a50f9cf98b1e380a55
Anas Bouzid <anasbouzid@gmail.com>
2023-05-10 13:16:43 -0500

    update demo

--- streamlit-code-editor/examples/demo.py
+++ streamlit-code-editor/examples/demo.py
@@ -43,13 +43,16 @@ language="python"
 theme="default"
 shortcuts="vscode"
 focus=False
+wrap=True
 btns = custom_buttons_alt
 
 st.markdown('<h1><a href="https://github.com/bouzidanas/streamlit.io/tree/master/streamlit-code-editor">Streamlit Code Editor</a> Demo</h1>', unsafe_allow_html=True)
 st.write("")
 with st.expander("Settings", expanded=True):
-    col_a, col_b, col_c = st.columns([4,12,3])
+    col_a, col_b, col_c, col_cb = st.columns([5,10,3,3])
     col_c.markdown('<div style="height: 2.5rem;"><br/></div>', unsafe_allow_html=True)
+    col_cb.markdown('<div style="height: 2.5rem;"><br/></div>', unsafe_allow_html=True)
+
     height_type = col_a.selectbox("height format:", ["css", "max lines", "min-max lines"], index=2)
     if height_type == "css":
         height = col_b.text_input("height (CSS):", "400px")
@@ -63,6 +66,7 @@ with st.expander("Settings", expanded=True):
     theme = col_e.selectbox("theme:", ["default", "light", "dark", "contrast"])
     shortcuts = col_f.selectbox("shortcuts:", ["emacs", "vim", "vscode", "sublime"], index=2)
     focus = col_c.checkbox("focus", False)
+    wrap = col_cb.checkbox("wrap", True)
 
 with st.expander("Components"):
     c_buttons = st.checkbox("custom buttons (JSON)", False)
@@ -86,7 +90,7 @@ with st.expander("Components"):
 st.write("### Output:")
 # construct props dictionary (->Ace Editor)
 ace_props = {"style": {"borderRadius": "0px 0px 8px 8px"}}
-response_dict = code_editor(demo_sample_python_code,  height = height, lang=language, theme=theme, shortcuts=shortcuts, focus=focus, buttons=btns, info=info_bar, props=ace_props)
+response_dict = code_editor(demo_sample_python_code,  height = height, lang=language, theme=theme, shortcuts=shortcuts, focus=focus, buttons=btns, info=info_bar, props=ace_props, options={"wrap": wrap})
 
 if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
     st.write("Response type: ", response_dict['type'])
