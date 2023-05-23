7f4695cf269365cab6f0d877c8c478091d032010
Anas Bouzid <anasbouzid@gmail.com>
2023-05-23 11:20:51 -0500

    fix for incorrect session state value

--- streamlit-reveal-slides/examples/markdown_playground.py
+++ streamlit-reveal-slides/examples/markdown_playground.py
@@ -128,5 +128,5 @@ state = rs.slides(sample_markdown,
                     markdown_props={"data-separator-vertical":"^--$"}, 
                     )
 
-if state:
+if state and state != { "indexh": -1, "indexv": -1, "indexf": -1, "paused": False, "overview": False}:
     st.session_state['reveal'] = state
